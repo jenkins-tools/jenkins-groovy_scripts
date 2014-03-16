ac123658ceb346c02198a1836bf66cd396b06571
Sunjoo Park <all4dich@gmail.com>
2014-03-16 10:26:35 +0900

    Import label change mechanism
    1. Change 'slaves' from 'string' type to 'List'
    2. Change hudson.model.Hudson to jenkins.model.Jenkins.*
    3. Remove 'string matching' step to find a node listed in targets
        Use getNode() function

--- change_node_labels.groovy
+++ change_node_labels.groovy
@@ -8,14 +8,14 @@ def build = thr?.executable
 
 // get parameters
 def parameters = build?.actions.find{ it instanceof ParametersAction }?.parameters
-def slaves = "" 
+def slaves = [] 
 def label_contents =""
 
 parameters.each {
     if ( it.name.contains('swfarm-oebuild'))
     {
         if (it.value) {
-           slaves = slaves+" "+it.name
+           slaves.add(it.name)
         }
     }  else if ( it.name.contains('Reserve or Free') ) {
         if ( it.value.contains('Reserve for official/integrate builds')) {
@@ -30,9 +30,7 @@ parameters.each {
 
 println slaves
 
-for (slave in hudson.model.Hudson.instance.slaves) {
-    oldSlaveName = slave.name
-    if (slaves.contains(oldSlaveName)) {
-        slave.setLabelString(label_contents)
-    }
+slaves.each{
+    node = jenkins.model.Jenkins.instance.getNode(it)
+    node.setLabelString(label_contents)
 }
