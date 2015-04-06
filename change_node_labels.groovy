f9e8ea57a0b33f411be0938b398d3571e62c304b
Sunjoo Park <allessunjoo.park@lge.com>
2015-04-06 18:38:19 +0900

    change_node_labels:Update with getParameter method

--- change_node_labels.groovy
+++ change_node_labels.groovy
@@ -15,35 +15,31 @@ def parameters = build?.actions.find{ it instanceof ParametersAction }?.paramete
 def slaves = []
 def label_contents =""
 def serverPrefix = ""
+def labelMode = ""
+
+def getParameter = { paramName ->
+    parameters.findAll {
+        it.name == paramName
+    }[0].value
+}
 
 //Reserve for only verification builds
-parameters.each {
-    if ( it.name.contains('swfarm-gateuobl'))
-    {
-        if (it.value) {
-            //slaves.add(it.name)
-            println "NODE : ${it.name} - just displayed"
-        }
-    }  else if ( it.name.contains('Reserve or Free') ) {
-        if ( it.value.contains('Reserve for official/integrate builds')) {
-            label_contents = 'integration-build official-build'
-        } else if ( it.value.contains('Free for all builds')) {
-            label_contents = 'official-build verification integration-build engineering-build'
-        } else if ( it.value.contains('Reserve for only verification builds')) {
-            label_contents = 'verification'
-        } else if ( it.value.contains('NONE')) {
-            label_contents = ''
-        }
-    } else if ( it.name.contains('RANGE_START') ) {
-        range_start = it.value.toInteger()
 
-    } else if ( it.name.contains('RANGE_END') ) {
-
-        range_end = it.value.toInteger()
-    } else if ( it.name.contains('SERVER_PREFIX') ) {
-        serverPrefix = it.value
-    }
+labelMode = getParameter("Reserve or Free")
+if (labelMode=="Reserve for official/integrate builds"){
+    label_contents = 'integration-build official-build'
+} else if ( labelMode=='Free for all builds') {
+    label_contents = 'official-build verification integration-build engineering-build'
+} else if ( labelMode =='Reserve for only verification builds') {
+    label_contents = 'verification'
+}else if ( labelMode=='NONE') {
+    label_contents = ''
 }
+
+range_start=getParameter('RANGE_START').toInteger()
+range_end=getParameter('RANGE_END').toInteger()
+serverPrefix=getParameter('SERVER_PREFIX')
+
 
 println "START : ${range_start}"
 println "END: ${range_end}"
@@ -56,10 +52,10 @@ for ( x in range_start..range_end	 ) {
 }
 
 println(label_contents)
+println slaves
 slaves.each{
     try {
         node  = jenkins.model.Jenkins.instance.getNode(it)
-
         node.setLabelString(label_contents)
     } catch (Exception e)
     {
