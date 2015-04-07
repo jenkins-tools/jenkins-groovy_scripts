e4174df963d6a12c6c21e80f5660d2820f6ea700
Sunjoo Park <allessunjoo.park@lge.com>
2015-04-07 10:14:39 +0900

    run-commands:ADD Thread

--- run-commands.groovy
+++ run-commands.groovy
@@ -1,5 +1,33 @@
 import hudson.model.*
 import jenkins.model.Jenkins
+import groovy.transform.Synchronized
+
+class MyData {
+    def test = []
+
+    @Synchronized
+    String getItem(){
+        def rValue = test.get(0)
+        test.remove(0)
+        return rValue
+
+    }
+
+    void addItem(String node){
+        def jenkinsNode = Jenkins.instance.getNode(node)
+        if (jenkinsNode != null){
+            def launcher=jenkinsNode.launcher
+            def nodeIp = launcher.host
+            test.add(nodeIp)
+        }
+    }
+
+    int size(){
+        return test.size()
+    }
+
+}
+
 
 def jenkins = Jenkins.instance
 // get current thread / Executor
@@ -28,21 +56,45 @@ range_end = getParameter("RANGE_END")
 serverPrefix = getParameter("SERVER_PREFIX")
 commands= getParameter("COMMANDS")
 
-(range_start.toInteger()..range_end.toInteger()).each{
-    slaves.add(serverPrefix+it)
-}
+def myData = new MyData()
 
+(range_start.toInteger()..range_end.toInteger()).each
+        {
+            println "IT : " + it
+            slaves.add(serverPrefix+it)
+            myData.addItem(serverPrefix+it)
+            println "end : " + it
+        }
+
+
+println "SIZE : " + myData.size()
 
 def runCommand = { targetnode, targetcommands ->
-    def jenkinsNode = jenkins.getNode(targetnode)
-    if ( jenkinsNode != null ) {
-        def launcher = jenkinsNode.launcher
-        def mhost = launcher.getHost()
-        println "INFO : Run commmands - ${targetcommands} - on ${targetnode}:${mhost}"
-        println "ssh ${mhost} ${targetcommands}".execute().text
+    def threadName = Thread.currentThread().getName()
+    println "INFO : ${threadName} : Run commmands - ${targetcommands} - on ${targetnode}"
+    println "INFO : ${threadName} : Result : "+"ssh ${targetnode} ${targetcommands}".execute().text
+    println "INFO : ${threadName} : END"
+    return Thread.currentThread().getName() +":Return Value"
+}
+
+def threads = []
+10.times {
+    threads << Thread.start {
+        def exitValue = "false"
+        while(exitValue=="false"){
+            try{
+                def testvalue=myData.getItem()
+                println Thread.currentThread().getName() + ":" + testvalue
+                def testString = runCommand(testvalue, "hostname")
+                println testString
+
+            }catch(ex){
+                exitValue="true"
+            }
+        }
     }
 }
 
-slaves.each{
-    runCommand(it, commands)
+threads.each{
+    it.join()
 }
